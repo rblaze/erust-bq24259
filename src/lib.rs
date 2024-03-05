09ed070f14b59529bf2ecb597b0b94ff4032e9c3
Andrey Sverdlichenko <blaze@rusty.zone>
2024-03-04 21:39:46 -0500

    Add more register definitions

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 
 pub mod registers;
 
+use bilge::arbitrary_int::u5;
 use embedded_hal::i2c::I2c;
 
 const ADDR: u8 = 0x6b;
@@ -17,17 +18,47 @@ impl<I2C: I2c> BQ24259<I2C> {
     }
 
     pub fn disable_watchdog(&mut self) -> Result<(), I2C::Error> {
-        self.update(5, |v| v & 0b11001111)
+        self.update(registers::REG_TERM_TIMER_CONTROL, |v| {
+            let mut reg = registers::ChargeTerminationTimerControl::from(v);
+            reg.set_en_timer(false);
+            reg.into()
+        })
     }
 
     pub fn status(&mut self) -> Result<registers::SystemStatus, I2C::Error> {
         let value = self.read(registers::REG_SYSTEM_STATUS)?;
-        Ok(registers::SystemStatus(value))
+        Ok(registers::SystemStatus::from(value))
     }
 
     pub fn new_fault(&mut self) -> Result<registers::NewFault, I2C::Error> {
         let value = self.read(registers::REG_NEW_FAULT)?;
-        Ok(registers::NewFault(value))
+        Ok(registers::NewFault::from(value))
+    }
+
+    fn ichg_to_milliamps(ichg: u5) -> u16 {
+        u16::from(ichg) * 64 + 512
+    }
+
+    fn milliamps_to_ichg(milliamps: u16) -> u5 {
+        let value = (milliamps.clamp(512, 2048) - 512) / 64;
+        u5::new(value as u8)
+    }
+
+    /// Gets charge current limit ICHG
+    pub fn charge_current_limit(&mut self) -> Result<u16, I2C::Error> {
+        let value = self.read(registers::REG_CHARGE_CURRENT_CONTROL)?;
+        let reg = registers::ChargeCurrentControl::from(value);
+        Ok(Self::ichg_to_milliamps(reg.ichg()))
+    }
+
+    /// Sets charge current limit ICHG, clamped to [512..2048] mA
+    pub fn set_charge_current_limit(&mut self, milliamps: u16) -> Result<(), I2C::Error> {
+        let ichg = Self::milliamps_to_ichg(milliamps);
+        self.update(registers::REG_CHARGE_CURRENT_CONTROL, |v| {
+            let mut reg = registers::ChargeCurrentControl::from(v);
+            reg.set_ichg(ichg);
+            reg.into()
+        })
     }
 
     // Reads register and returns its value.
@@ -38,8 +69,70 @@ impl<I2C: I2c> BQ24259<I2C> {
     }
 
     // Reads register, updates it and writes back.
-    pub fn update(&mut self, register: u8, update: fn(u8) -> u8) -> Result<(), I2C::Error> {
+    pub fn update(&mut self, register: u8, update: impl Fn(u8) -> u8) -> Result<(), I2C::Error> {
         let value = self.read(register)?;
         self.i2c.write(ADDR, &[register, update(value)])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use embedded_hal::i2c::{Error, ErrorType};
+
+    #[derive(Debug)]
+    enum DummyError {}
+
+    impl Error for DummyError {
+        fn kind(&self) -> embedded_hal::i2c::ErrorKind {
+            unimplemented!()
+        }
+    }
+
+    struct DummyBus {}
+
+    impl ErrorType for DummyBus {
+        type Error = DummyError;
+    }
+
+    impl I2c for DummyBus {
+        fn transaction(
+            &mut self,
+            _address: u8,
+            _operations: &mut [embedded_hal::i2c::Operation<'_>],
+        ) -> Result<(), Self::Error> {
+            unimplemented!()
+        }
+    }
+
+    #[test]
+    fn ichg_read() {
+        // Default value means 2048
+        assert_eq!(
+            BQ24259::<DummyBus>::ichg_to_milliamps(u5::new(0b11000)),
+            2048
+        );
+        assert_eq!(BQ24259::<DummyBus>::ichg_to_milliamps(u5::new(0b01100)), 1280);
+        // Zero value means 512
+        assert_eq!(BQ24259::<DummyBus>::ichg_to_milliamps(u5::new(0)), 512);
+    }
+
+    #[test]
+    fn ichg_write() {
+        // Default value means 2048
+        assert_eq!(
+            BQ24259::<DummyBus>::milliamps_to_ichg(2048).value(),
+            0b11000
+        );
+        assert_eq!(BQ24259::<DummyBus>::milliamps_to_ichg(896).value(), 0b00110);
+        // Zero value means 512
+        assert_eq!(BQ24259::<DummyBus>::milliamps_to_ichg(512).value(), 0);
+        // Value clumps to 2048 from the top
+        assert_eq!(
+            BQ24259::<DummyBus>::milliamps_to_ichg(10000).value(),
+            0b11000
+        );
+        // Value clumps to 512 from the bottom
+        assert_eq!(BQ24259::<DummyBus>::milliamps_to_ichg(5).value(), 0);
     }
 }
--- src/registers.rs
+++ src/registers.rs
@@ -1,6 +1,8 @@
+#![deny(unsafe_code)]
+
 //! I2C control and status registers
 
-use bitfield::bitfield;
+use bilge::prelude::*;
 
 /// Input Source Control Register REG00
 pub const REG_INPUT_SOURCE_CONTROL: u8 = 0x00;
@@ -11,6 +13,15 @@ pub const REG_POWER_ON_CONFIGURATION: u8 = 0x01;
 /// Charge Current Control Register REG02
 pub const REG_CHARGE_CURRENT_CONTROL: u8 = 0x02;
 
+#[bitsize(8)]
+#[derive(FromBits)]
+pub struct ChargeCurrentControl {
+    pub force_20pct: bool,
+    pub bcold: bool,
+    pub ichg: u5,
+    pub reserved: u1,
+}
+
 /// Pre-Charge/Termination Current Control Register REG03
 pub const REG_PCT_CURRENT_CONTROL: u8 = 0x03;
 
@@ -19,6 +30,36 @@ pub const REG_CHARGE_VOLTAGE_CONTROL: u8 = 0x04;
 
 /// Charge Termination/Timer Control Register REG05
 pub const REG_TERM_TIMER_CONTROL: u8 = 0x05;
+
+#[bitsize(2)]
+#[derive(FromBits, Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum ChgTimer {
+    FiveHours = 0,
+    EightHours = 1,
+    TwelveHours = 2,
+    TwentyHours = 3,
+}
+
+#[bitsize(2)]
+#[derive(FromBits, Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Watchdog {
+    Disabled = 0,
+    Sec40 = 1,
+    Sec80 = 2,
+    Sec160 = 3,
+}
+
+/// Charge Termination/Timer Control Register
+#[bitsize(8)]
+#[derive(FromBits, DebugBits)]
+pub struct ChargeTerminationTimerControl {
+    reserved: u1,
+    pub charge_timer: ChgTimer,
+    pub en_timer: bool,
+    pub watchdog: Watchdog,
+    reserved: u1,
+    pub en_term: bool,
+}
 
 /// Boost Voltage/Thermal Regulation Control Register REG06
 pub const REG_BOOST_TEMP_CONTROL: u8 = 0x06;
@@ -29,32 +70,108 @@ pub const REG_MISC_OPERATION_CONTROL: u8 = 0x07;
 /// System Status Register REG08
 pub const REG_SYSTEM_STATUS: u8 = 0x08;
 
-bitfield! {
-    /// System Status Register description
-    pub struct SystemStatus(u8);
-    impl Debug;
-    pub vbus_stat, _: 7,6;
-    pub chrg_stat, _: 5,4;
-    pub dpm_stat, _: 3;
-    pub pg_stat, _: 2;
-    pub therm_stat, _: 1;
-    pub vsys_stat, _: 0;
+#[bitsize(2)]
+#[derive(FromBits, Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Vbus {
+    Unknown = 0,
+    UsbHost = 1,
+    AdapterPort = 2,
+    Otg = 3,
+}
+
+#[bitsize(2)]
+#[derive(FromBits, Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Chrg {
+    NotCharging = 0,
+    PreCharge = 1,
+    FastCharging = 2,
+    ChargeTermination = 3,
+}
+
+/// System Status Register
+#[bitsize(8)]
+#[derive(FromBits, DebugBits)]
+pub struct SystemStatus {
+    pub vsys: bool,
+    pub therm: bool,
+    pub pg: bool,
+    pub dpm: bool,
+    pub chrg: Chrg,
+    pub vbus: Vbus,
 }
 
 /// New Fault Register REG09
 pub const REG_NEW_FAULT: u8 = 0x09;
 
-bitfield! {
-    /// System Status Register description
-    pub struct NewFault(u8);
-    impl Debug;
-    pub watchdog_fault, _: 7;
-    pub otg_fault, _: 6;
-    pub chrg_fault, _: 5, 4;
-    pub bat_fault, _: 3;
-    pub ntc_fault_cold, _: 1;
-    pub ntc_fault_hot, _: 0;
+#[bitsize(2)]
+#[derive(FromBits, Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum ChrgFault {
+    Normal = 0,
+    /// OVP or bad source
+    InputFault = 1,
+    ThermalShutdown = 2,
+    ChargeTimerExpiration = 3,
+}
+
+/// New Fault Register
+#[bitsize(8)]
+#[derive(FromBits, DebugBits)]
+pub struct NewFault {
+    pub ntc_hot: bool,
+    pub ntc_cold: bool,
+    reserved: u1,
+    pub bat_fault: bool,
+    pub chrg_fault: ChrgFault,
+    pub otg_fault: bool,
+    pub watchdog_expired: bool,
 }
 
 /// Vender / Part / Revision Status Register REG0A
 pub const REG_VENDOR: u8 = 0x0A;
+
+/// Expected value of REG_VENDOR
+pub const EXPECTED_VENDOR_VALUE: u8 = 0b00100000;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn charge_current_control() {
+        let reg = ChargeCurrentControl::from(0b01011001);
+        assert_eq!(u8::from(reg.ichg()), 0b10110);
+        assert_eq!(reg.bcold(), false);
+        assert_eq!(reg.force_20pct(), true);
+    }
+
+    #[test]
+    fn charge_termination_timer_control() {
+        let reg = ChargeTerminationTimerControl::from(0b10011100);
+        assert_eq!(reg.en_term(), true);
+        assert_eq!(reg.watchdog(), Watchdog::Sec40);
+        assert_eq!(reg.en_timer(), true);
+        assert_eq!(reg.charge_timer(), ChgTimer::TwelveHours);
+    }
+
+    #[test]
+    fn status() {
+        let reg = SystemStatus::from(0b10010100);
+        assert_eq!(reg.vbus(), Vbus::AdapterPort);
+        assert_eq!(reg.chrg(), Chrg::PreCharge);
+        assert_eq!(reg.dpm(), false);
+        assert_eq!(reg.pg(), true);
+        assert_eq!(reg.therm(), false);
+        assert_eq!(reg.vsys(), false);
+    }
+
+    #[test]
+    fn new_fault() {
+        let reg = NewFault::from(0b10100010);
+        assert_eq!(reg.watchdog_expired(), true);
+        assert_eq!(reg.otg_fault(), false);
+        assert_eq!(reg.chrg_fault(), ChrgFault::ThermalShutdown);
+        assert_eq!(reg.bat_fault(), false);
+        assert_eq!(reg.ntc_cold(), true);
+        assert_eq!(reg.ntc_hot(), false);
+    }
+}
